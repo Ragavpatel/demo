9dab796e35c531103e95a62616b3011bdb9c78a6
Ragavpatel <Ragav@primeqasolutions.com>
2025-06-18 19:07:13 +0530

    update base

---
+++ DemoTest/src/main/java/base/BaseTest.java
@@ -0,0 +1,34 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://qa-takehome.dtxplus.com/");
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    @AfterClass
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+} 
